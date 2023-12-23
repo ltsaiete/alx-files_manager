1906dbe873f521d0e57eb19a8b09e024bfde951c
Luis Saiete <luistsaiete@gmail.com>
2023-12-24 00:04:24 +0200

    make methods static

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,15 +2,15 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-	getStatus(request, response) {
-		return response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
-	}
+  static getStatus(request, response) {
+    return response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+  }
 
-	async getStats(request, response) {
-		const users = await dbClient.nbUsers();
-		const files = await dbClient.nbFiles();
-		return response.status(200).json({ users, files });
-	}
+  static async getStats(request, response) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    return response.status(200).json({ users, files });
+  }
 }
 
-export default new AppController();
+export default AppController;
