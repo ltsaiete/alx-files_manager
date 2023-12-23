1d49b313e827ef7bceba3ebb2b851b8cfa79e8a2
Luis Saiete <luistsaiete@gmail.com>
2023-12-23 10:03:58 +0200

    Fix eslint

--- utils/db.js
+++ utils/db.js
@@ -1,66 +1,66 @@
 import 'dotenv/config';
 import { MongoClient } from 'mongodb';
 class DBClient {
-	#host = process.env.DB_HOST || 'localhost';
-	#port = process.env.DB_PORT || 27017;
-	#dbName = process.env.DB_DATABASE || 'files_manager';
-	#connected = false;
-	#db;
-	constructor() {
-		const url = `mongodb://${this.#host}:${this.#port}`;
-		MongoClient.connect(url, (err, client) => {
-			if (!err) {
-				console.log('Mongo client ready');
-				this.#db = client.db(this.#dbName);
-				this.#connected = true;
-			} else {
-				console.log('Error connecting to Mongo');
-			}
-		});
-	}
-	isAlive() {
-		return this.#connected;
-	}
+  #host = process.env.DB_HOST || 'localhost';
+  #port = process.env.DB_PORT || 27017;
+  #dbName = process.env.DB_DATABASE || 'files_manager';
+  #connected = false;
+  #db;
+  constructor() {
+    const url = `mongodb://${this.#host}:${this.#port}`;
+    MongoClient.connect(url, (err, client) => {
+      if (!err) {
+        console.log('Mongo client ready');
+        this.#db = client.db(this.#dbName);
+        this.#connected = true;
+      } else {
+        console.log('Error connecting to Mongo');
+      }
+    });
+  }
+  isAlive() {
+    return this.#connected;
+  }
 
-	async nbUsers() {
-		const collection = this.#db.collection('users');
-		const count = await collection.countDocuments();
-		return count;
-	}
+  async nbUsers() {
+    const collection = this.#db.collection('users');
+    const count = await collection.countDocuments();
+    return count;
+  }
 
-	async nbFiles() {
-		const collection = this.#db.collection('files');
-		const count = await collection.countDocuments();
-		return count;
-	}
+  async nbFiles() {
+    const collection = this.#db.collection('files');
+    const count = await collection.countDocuments();
+    return count;
+  }
 
-	waitConnection() {
-		return new Promise((resolve, reject) => {
-			let i = 0;
-			const repeatFct = async () => {
-				setTimeout(() => {
-					i += 1;
-					if (i >= 10) {
-						reject();
-					} else if (!this.isAlive()) {
-						repeatFct();
-					} else {
-						resolve();
-					}
-				}, 1000);
-			};
-			repeatFct();
-		});
-	}
+  waitConnection() {
+    return new Promise((resolve, reject) => {
+      let i = 0;
+      const repeatFct = async () => {
+        setTimeout(() => {
+          i += 1;
+          if (i >= 10) {
+            reject();
+          } else if (!this.isAlive()) {
+            repeatFct();
+          } else {
+            resolve();
+          }
+        }, 1000);
+      };
+      repeatFct();
+    });
+  }
 
-	async collection(name) {
-		await this.waitConnection();
-		return this.#db.collection(name);
-	}
+  async collection(name) {
+    await this.waitConnection();
+    return this.#db.collection(name);
+  }
 
-	get client() {
-		return this.#db;
-	}
+  get client() {
+    return this.#db;
+  }
 }
 
 const dbClient = new DBClient();
--- utils/redis.js
+++ utils/redis.js
@@ -1,46 +1,41 @@
-import { createClient } from 'redis';
-import { promisify } from 'util';
+import { createClient } from 'redis'
+import { promisify } from 'util'
 
 class RedisClient {
-	client;
-	get;
-	connected = false;
+  constructor() {
+    this.client = createClient()
+      .on('error', (error) => {
+        console.log(error)
+        this.connected = false
+      })
+      .on('ready', () => {
+        console.log('Redis client connection ready')
+        this.connected = true
+      })
 
-	constructor() {
-		// this.connected = true;
-		this.client = createClient()
-			.on('error', (error) => {
-				console.log(error);
-				this.connected = false;
-			})
-			.on('ready', () => {
-				console.log('Redis client connection ready');
-				this.connected = true;
-			});
+    console.log(this.client.connected)
 
-		console.log(this.client.connected);
+    this.get = promisify(this.client.get).bind(this.client)
+  }
 
-		this.get = promisify(this.client.get).bind(this.client);
-	}
+  async get(key) {
+    const value = await this.get(key).then((response) => response)
+    return value
+  }
 
-	async get(key) {
-		const value = await this.get(key).then((response) => response);
-		return value;
-	}
+  async set(key, value, duration) {
+    await this.client.set(key, value, 'EX', duration)
+  }
 
-	async set(key, value, duration) {
-		await this.client.set(key, value, 'EX', duration);
-	}
+  async del(key) {
+    await this.client.del(key)
+  }
 
-	async del(key) {
-		await this.client.del(key);
-	}
-
-	isAlive() {
-		return this.client.connected;
-	}
+  isAlive() {
+    return this.client.connected
+  }
 }
 
-const redisClient = new RedisClient();
+const redisClient = new RedisClient()
 
-export default redisClient;
+export default redisClient
