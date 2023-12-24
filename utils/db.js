c44ab2306743d79a98b716ba9623a30f9278e008
Luis Saiete <luistsaiete@gmail.com>
2023-12-25 00:16:53 +0200

    Change db.js

--- utils/db.js
+++ utils/db.js
@@ -5,42 +5,31 @@ class DBClient {
     const DB_HOST = process.env.DB_HOST || 'localhost';
     const DB_PORT = process.env.DB_PORT || 27017;
     const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
-    const url = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+    const url = `mongodb://${DB_HOST}:${DB_PORT}`;
     this.connected = false;
-
-    this.client = new MongoClient.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
+    MongoClient.connect(url, (err, client) => {
+      if (!err) {
+        console.log('Mongo client ready');
+        this.db = client.db(DB_DATABASE);
+        this.connected = true;
+      } else {
+        console.log('Error connecting to Mongo');
+      }
     });
-    this.client.connect();
-    this.db = this.client.db();
-    // this.client = new MongoClient.connect(url, (err, client) => {
-    //   if (!err) {
-    //     console.log('Mongo client ready');
-    //     this.client = client.db(DB_DATABASE);
-    //     this.connected = true;
-    //   } else {
-    //     console.log('Error connecting to Mongo');
-    //   }
-    // });
   }
-
   isAlive() {
     return this.connected;
   }
-
   async nbUsers() {
     const collection = this.db.collection('users');
     const count = await collection.countDocuments();
     return count;
   }
-
   async nbFiles() {
     const collection = this.db.collection('files');
     const count = await collection.countDocuments();
     return count;
   }
-
   waitConnection() {
     return new Promise((resolve, reject) => {
       let i = 0;
@@ -59,17 +48,13 @@ class DBClient {
       repeatFct();
     });
   }
-
   async collection(name) {
     await this.waitConnection();
     return this.db.collection(name);
   }
-
   get client() {
-    return this.client;
+    return this.db;
   }
 }
-
 const dbClient = new DBClient();
-
 export default dbClient;
