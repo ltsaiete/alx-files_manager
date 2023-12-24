d59c30654ca731772ac7a9b5a2280b0ef45ad097
Luis Saiete <luistsaiete@gmail.com>
2023-12-25 00:47:32 +0200

    Update UserController

---
+++ controllers/UsersController.js
@@ -0,0 +1,20 @@
+import sha1 from 'sha1';
+import userRepository from '../repositories/User';
+
+class UsersController {
+  async postNew(request, response) {
+    const { email, password } = request.body;
+
+    if (!email) return response.status(400).json({ error: 'Missing email' });
+    if (!password) return response.status(400).json({ error: 'Missing password' });
+
+    const userExists = await userRepository.getUserByEmail(email);
+    if (userExists) return response.status(400).json({ error: 'Already exist' });
+
+    const userId = await userRepository.insertUser(email, password);
+
+    return response.status(201).json({ id: userId, email });
+  }
+}
+
+export default new UsersController();
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
-// import UserController from '../controllers/User';
+import UsersController from '../controllers/UsersController';
 
 const routes = Router();
 
 routes.get('/status', AppController.getStatus);
 routes.get('/stats', AppController.getStats);
 
-// routes.post('/users', UserController.postNew);
+routes.post('/users', UsersController.postNew);
 
 export default routes;
