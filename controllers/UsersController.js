c9dd4fa34aaa51f4bee695ca2062f48f52e025ef
Luis Saiete <luistsaiete@gmail.com>
2023-12-25 10:57:07 +0200

    Remove private property in userRepository

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,4 +1,4 @@
-// import userRepository from '../repositories/User';
+import userRepository from '../repositories/User';
 
 class UsersController {
   async postNew(request, response) {
@@ -7,11 +7,10 @@ class UsersController {
     if (!email) return response.status(400).json({ error: 'Missing email' });
     if (!password) return response.status(400).json({ error: 'Missing password' });
 
-    // const userExists = await userRepository.getUserByEmail(email);
-    // if (userExists) return response.status(400).json({ error: 'Already exist' });
+    const userExists = await userRepository.getUserByEmail(email);
+    if (userExists) return response.status(400).json({ error: 'Already exist' });
 
-    // const userId = await userRepository.insertUser(email, password);
-    const userId = '658942e64d983e1f24558d1a';
+    const userId = await userRepository.insertUser(email, password);
 
     return response.status(201).json({ id: userId, email });
   }
--- repositories/User.js
+++ repositories/User.js
@@ -2,23 +2,22 @@ import sha1 from 'sha1';
 import dbClient from '../utils/db';
 
 class UserRepository {
-	#collection;
-	constructor() {
-		dbClient.collection('users').then((collection) => {
-			this.#collection = collection;
-		});
-	}
+  constructor() {
+    dbClient.collection('users').then((collection) => {
+      this.collection = collection;
+    });
+  }
 
-	async getUserByEmail(email) {
-		const user = await this.#collection.findOne({ email });
-		return user;
-	}
+  async getUserByEmail(email) {
+    const user = await this.collection.findOne({ email });
+    return user;
+  }
 
-	async insertUser(email, password) {
-		const password_hash = sha1(password);
-		const result = await this.#collection.insertOne({ email, password: password_hash });
-		return result.insertedId;
-	}
+  async insertUser(email, password) {
+    const password_hash = sha1(password);
+    const result = await this.collection.insertOne({ email, password: password_hash });
+    return result.insertedId;
+  }
 }
 
 export default new UserRepository();
