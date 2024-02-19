43a6801f74291b7665c54bdb31ebe49d0ffb91c1
Luis Saiete <luistsaiete@gmail.com>
2024-02-19 10:44:03 +0200

    Use fs promises

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-import redisClient from './utils/redis';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
 
-(async () => {
-	console.log(redisClient.isAlive());
-	console.log(await redisClient.get('myKey'));
-	await redisClient.set('myKey', 12, 5);
-	console.log(await redisClient.get('myKey'));
+chai.use(chaiHttp);
 
-	setTimeout(async () => {
-		console.log(await redisClient.get('myKey'));
-		console.log(redisClient.isAlive());
-	}, 1000 * 10);
-})();
+describe('GET /status', () => {
+  it('GET /status exists', (done) => {
+    chai
+      .request('http://localhost:5000')
+      .get('/status')
+      .end((err, res) => {
+        chai.expect(err).to.be.null;
+        chai.expect(res).to.have.status(200);
+        done();
+      });
+  }).timeout(30000);
+});
